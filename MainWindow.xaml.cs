6e1c822584bc009f8d11293e73a0da1784dd1bcf
= <=>
2026-04-13 09:16:57 +1200

    Created Basic UI Design with Sample Data

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,8 +16,34 @@ namespace Assessment1;
 /// </summary>
 public partial class MainWindow : Window
 {
+	private LinkedList<Movie> movies = new LinkedList<Movie>(); 
     public MainWindow()
     {
         InitializeComponent();
+		SampleData();  
+		dtgMovies.ItemsSource = movies; 
     }
+
+	public IEnumerable<Movie> GetAll() 
+	{ 
+		return movies; 
+	} 
+
+	private void SampleData()
+	{ 
+		movies.AddLast(new Movie() { ID = "M1", Title = "Back to the Future", Director = "Robert Zemeckis", Genre = "Sci-Fi", Year = 1985, Availability = "Available"}); 
+		movies.AddLast(new Movie() { ID = "M2", Title = "Star Wars", Director = "George Lucas", Genre = "Sci-Fi", Year = 1977, Availability = "Available"}); 
+		movies.AddLast(new Movie() { ID = "M3", Title = "That Darn Cat!", Director = "Robert Stevenson", Genre = "Comedy", Year = 1965, Availability = "Available"}); 
+		movies.AddLast(new Movie() { ID = "M4", Title = "The Final Countdown", Director = "Don Taylor", Genre = "Sci-Fi", Year = 1980, Availability = "Available"}); 
+
+	} 
 }
+
+public class Movie { 
+	public string ID { get; set; } 
+	public string Title { get; set; } 
+	public string Director { get; set; } 
+	public string Genre { get; set; } 
+	public int Year { get; set; } 
+	public string Availability { get; set; } 
+} 
